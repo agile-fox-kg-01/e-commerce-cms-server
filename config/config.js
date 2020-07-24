e3839b5199e5a982bb2abece1f434daa18af44df
julioj27 <juliojaya27@gmail.com>
2020-07-24 20:30:41 +0700

    add config for deploy server

--- config/config.js
+++ config/config.js
@@ -18,10 +18,6 @@ module.exports = {
     "dialect": "postgres"
   },
   "production": {
-    "username": "postgres",
-    "password": "12345678!",
-    "database": "database_production",
-    "host": "localhost",
-    "dialect": "postgres"
+    "use_env_variable": "DATABASE_URL"
   }
 }
