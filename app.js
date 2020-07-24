9d08413ad274382d02f7f97747c39d48e4a2f280
julioj27 <juliojaya27@gmail.com>
2020-07-24 20:54:14 +0700

    add port for production

--- app.js
+++ app.js
@@ -14,4 +14,11 @@ app.use(cors());
 app.use('/', router);
 app.use(errorHandler);
 
+const PORT = process.env.PORT || 3000;
+if(process.env.NODE_ENV === 'production') {
+    app.listen(PORT, () => {
+        console.log(`Server is running at PORT ${PORT}`);
+    })
+}
+
 module.exports = app;
