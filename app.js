469f900d0f2e7233096920d6f76329dad6fcd266
aarsandi <aarsandi@gmail.com>
2020-07-25 20:24:15 +0700

    kill some bug -_-

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const express = require('express')
 const app = express()
 const routes = require('./routes/index')
+const PORT = 3000
 const errorhandler = require('./middlewares/errorhandler')
 app.use(cors())
 
@@ -11,4 +12,7 @@ app.use(express.json())
 app.use('/', routes)
 app.use(errorhandler)
 
+app.listen(PORT, () => {
+    console.log(`running at port:${PORT}`)
+})
 // module.exports = app
