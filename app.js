2b8d01c2f44b3ba3bde5024117bc97535f39803b
aarsandi <aarsandi@gmail.com>
2020-07-25 18:20:07 +0700

    deploy heroku yeah

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ require('dotenv').config()
 const cors = require('cors')
 const express = require('express')
 const app = express()
-app.use(cors())
 const routes = require('./routes/index')
+const PORT = process.env.PORT
 const errorhandler = require('./middlewares/errorhandler')
+app.use(cors())
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use('/', routes)
 app.use(errorhandler)
 
-module.exports = app
+app.listen(PORT, () => {
+    console.log(`running at port:${PORT}`)
+})
+// module.exports = app
