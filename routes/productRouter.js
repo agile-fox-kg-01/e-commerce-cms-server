5d9de71f5890adcb841b6da31ed8586bc4c9ec71
ichlasulamalgit <id.ichlasulamal@gmail.com>
2020-07-25 15:50:01 +0700

    Succes to add Seed product, Authentication and authorization

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -2,10 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const ControllerProduct = require('../controllers/controllerProduct')
-
-router.get('/', ControllerProduct.getProducts)
-router.post('/', ControllerProduct.postProducts)
-router.patch('/:id', ControllerProduct.patchProducts)
-router.delete('/:id', ControllerProduct.deleteProducts)
+const { authentication, isAdmin } = require('../midlewares/auth')
+router.get('/', authentication, isAdmin, ControllerProduct.getProducts)
+router.post('/', authentication, isAdmin, ControllerProduct.postProducts)
+router.patch('/:id', authentication, isAdmin, ControllerProduct.patchProducts)
+router.delete('/:id', authentication, isAdmin, ControllerProduct.deleteProducts)
 
 module.exports = router
