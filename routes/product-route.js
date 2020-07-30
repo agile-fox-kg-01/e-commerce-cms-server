add02aa4f0ed893d11efc88c80f14e3906ae13ef
Timothy <timothi888@gmail.com>
2020-07-30 12:05:31 +0700

    add server

--- routes/product-route.js
+++ routes/product-route.js
@@ -4,7 +4,7 @@ const ProductController = require('../controller/product-controller.js')
 const {authentication, authorization} = require('../middleware/auth.js')
 
 router.post('/', authentication, authorization, ProductController.create)
-router.get('/', authentication, authorization, ProductController.read)
+router.get('/', ProductController.read)
 router.get('/:id', authentication, authorization, ProductController.readById)
 router.put('/:id', authentication, authorization, ProductController.update)
 router.delete('/:id', authentication, authorization, ProductController.delete)
